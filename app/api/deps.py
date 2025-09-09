b198d5bf644a8b7ac88d592e844dd0a27e987c0b
Vivek Yadav <129324717+vivek-iin@users.noreply.github.com>
2025-09-09 12:45:14 +0530

    Delete app directory

--- app/api/deps.py
+++
@@ -1,7 +0,0 @@
-# app/api/deps.py
-# =============================================================================
-from services.prediction_service import prediction_service
-
-def get_prediction_service():
-    """Dependency to get prediction service"""
-    return prediction_ser
